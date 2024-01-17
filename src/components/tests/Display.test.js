6188019b1d721f45323f44c8701e5097a350154f
Samuel Sanchez <wearycodes@gmail.com>
2024-01-16 21:29:56 -0600

    ALL DONE

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -2,9 +2,43 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
+import { waitFor } from '@testing-library/react';
 
-test('renders without errors with no props', async () => { });
+const exampleShow = {
+    name: 'Sam Show',
+    summary: 'THIS IS A SUMMARY OF SAMS SHOW',
+    seasons: [
+        {id: 1,
+        name: 'SEASON 1',
+        episodes: []
+        },
+        {id: 2,
+            name: 'SEASON 2',
+            episodes: []
+            }
+    ]
+}
 
-test('renders Show component when the button is clicked ', () => { });
 
-test('renders show season options matching your data when the button is clicked', () => { });
+
+test('renders without errors with no props', async () => {
+    render(<Display />)
+});
+
+test('renders Show component when the button is clicked ', async () => {
+    render(<Display />)
+    const button = screen.getByRole('button')
+    await fireEvent.click(button)
+    const show = await screen.findByTestId('show-container')
+    expect(show).toBeInTheDocument()
+});
+
+test('renders show season options matching your data when the button is clicked', async () => {
+    render(<Display/>)
+    const button = screen.getByRole('button')
+    await fireEvent.click(button)
+    await waitFor(() => {
+        const seasonOptions = screen.queryAllByTestId('season-option')
+        expect(seasonOptions).toHaveLength(5)
+    })
+});
--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
+import Episode from '../Episode';
 
-test('renders without errors', () => { });
+const exampleShow = {
+    name: 'Sam Show',
+    summary: 'THIS IS A SUMMARY OF SAMS SHOW',
+    seasons: [
+        {id: 1,
+        name: 'SEASON 1',
+        episodes: []
+        },
+        {id: 2,
+            name: 'SEASON 2',
+            episodes: []
+            }
+    ]
+}
 
-test('renders Loading component when prop show is null', () => { });
+test('renders without errors', () => {
+    render(<Show selectedSeason = {'none'} show = {exampleShow}/>)
+});
 
-test('renders same number of options seasons are passed in', () => { });
+test('renders Loading component when prop show is null', () => {
+    render(<Show show = {null}/>)
+    const value = screen.getByText('Fetching data...')
+    expect(value).toBeVisible()
+});
 
-test('handleSelect is called when an season is selected', () => { });
+test('renders same number of options seasons are passed in', () => {
+    render(<Show show = {exampleShow} selectedSeason={'none'} />)
+    const seasonOptions = screen.queryAllByTestId('season-option')
+    expect(seasonOptions.length).toEqual(2)
+});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+test('handleSelect is called when an season is selected', async () => {
+    const handleSelect = jest.fn()
+    render(<Show show = {exampleShow} selectedSeason={'none'} handleSelect = {handleSelect}/>)
+    const select = screen.getByLabelText(/Select A Season/i)
+    await userEvent.selectOptions(select, ['1'])
+    expect(handleSelect).toBeCalled()
+});
+
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+    const {rerender} = render(<Show show={exampleShow} selectedSeason={'none'}/>)
+    const episodes = screen.queryByTestId('episodes-container')
+    expect(episodes).not.toBeInTheDocument()
+
+        rerender(<Show show={exampleShow} selectedSeason = {1}/>)
+});
+
+// Build an example data structure that contains the show data in the correct format. 
+// A show should contain a name, a summary and an array of seasons, each with a id, name and an (empty) list of episodes 
+// within them. Use console.logs within the client code if you need to to verify the structure of show data.
+
